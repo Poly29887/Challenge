f67ef7a4ffc23907ab9f953cde357631b4cd404d
TANAPORN SRIBOONPHONG <tanapon1325@gmail.com>
2020-08-11 23:19:41 +0700

    Add project files.

---
+++ Challenge_loop_if_basicmath/Ch.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	long n1, n2,i,sum=0;
+	double avg, s_d;
+	scanf("%ld",&n1);
+	scanf("%ld", &n2);
+	if (n1 < n2)
+	{
+		for (i = n1; i <= n2; i++)
+		{
+			printf("%ld ", i);
+			sum+=
+		}
+
+	}
+
+}

c8804ba1c077ac247c3fd42aedfca9313299fbaa
TANAPORN SRIBOONPHONG <tanapon1325@gmail.com>
2020-08-12 00:24:26 +0700

    avg+sd

--- Challenge_loop_if_basicmath/Ch.cpp
+++ Challenge_loop_if_basicmath/Ch.cpp
@@ -1,18 +1,51 @@
 #include<stdio.h>
+#include<math.h>
 int main()
 {
 	long n1, n2,i,sum=0;
-	double avg, s_d;
+	double avg,ans,count=0,sumP=0;
+	printf("Input : ");
 	scanf("%ld",&n1);
 	scanf("%ld", &n2);
 	if (n1 < n2)
 	{
+		printf("Output : \n");
 		for (i = n1; i <= n2; i++)
 		{
 			printf("%ld ", i);
-			sum+=
+			sum += i;
+			count++;
 		}
+		avg = sum / count;
+		printf("\n%.1lf", avg);
+
+		for (i = n1; i <= n2; i++)
+		{
+			sumP += pow(i - avg, 2);
+		}
+		ans = sqrt(sumP / (count - 1));
+		printf("\n%.2lf", ans);
+	}
+	else
+	{
+		printf("Output : \n");
+		for (i = n1; i >= n2; i--)
+		{
+			printf("%ld ", i);
+			sum += i;
+			count++;
+		}
+		avg = sum / count;
+		printf("\n%.2lf", avg);
+		for (i = n1; i >= n2; i--)
+		{
+			sumP += pow(i - avg, 2);
+		}
+		ans = sqrt(sumP / (count - 1));
+		if (n1 == n2) ans = 0;
+		printf("\n%.2lf", ans);
 
 	}
+
 
 }
